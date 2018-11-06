858606470d459826eaf08e9ee7b796c3f492b0ad
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2018-11-06 23:33:47 +0100

    DLL build stuff

--- cmake-modules/molaBuildFlags.cmake
+++ cmake-modules/molaBuildFlags.cmake
@@ -1,7 +1,49 @@
 
 
+# Avoid the need for DLL export/import macros in Windows:
+if (WIN32)
+  set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS  ON)
+endif()
+
+# Detect wordsize:
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)  # Size in bytes!
+  set(MOLA_WORD_SIZE 64)
+else()
+  set(MOLA_WORD_SIZE 32)
+endif()
+
+# Compiler ID:
+if (MSVC)
+  # 1700 = VC 11.0 (2012)
+  # 1800 = VC 12.0 (2013)
+  #           ... (13 was skipped!)
+  # 1900 = VC 14.0 (2015)
+  # 1910 = VC 14.1 (2017)
+  math(EXPR MSVC_VERSION_3D "(${MSVC_VERSION}/10)-60")
+  if (MSVC_VERSION_3D GREATER 120)
+    math(EXPR MSVC_VERSION_3D "${MSVC_VERSION_3D}+10")
+  endif()
+  set(MOLA_COMPILER_NAME "msvc${MSVC_VERSION_3D}")
+else()
+  set(MOLA_COMPILER_NAME "${CMAKE_CXX_COMPILER_ID}")
+endif()
+
+# Build DLL full name:
+if (WIN32)
+  set(MOLA_DLL_VERSION_POSTFIX
+    "${MOLA_VERSION_NUMBER_MAJOR}${MOLA_VERSION_NUMBER_MINOR}${MOLA_VERSION_NUMBER_PATCH}_${MOLA_COMPILER_NAME}_x${MOLA_WORD_SIZE}")
+  message(STATUS "Using DLL version postfix: ${MOLA_DLL_VERSION_POSTFIX}")
+else()
+  set(MOLA_DLL_VERSION_POSTFIX "")
+endif()
+
+# Group projects in "folders"
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER "CMakeTargets")
+
 # We want libraries to be named "libXXX" in all compilers, "libXXX-dbg" in MSVC
 set(CMAKE_SHARED_LIBRARY_PREFIX "lib")
+set(CMAKE_IMPORT_LIBRARY_PREFIX "lib")
 set(CMAKE_STATIC_LIBRARY_PREFIX "lib")
 if(MSVC)
   set(CMAKE_DEBUG_POSTFIX "-dbg")
@@ -9,7 +51,11 @@ else()
   set(CMAKE_DEBUG_POSTFIX "")
 endif()
 
-# Set defaults for each project CMake target:
+# -----------------------------------------------------------------------------
+# mola_set_target_build_options(target)
+#
+# Set defaults for each MOLA cmake target
+# -----------------------------------------------------------------------------
 function(mola_set_target_build_options TARGETNAME)
   # Build for C++17
   # -------------------------
@@ -43,9 +89,14 @@ function(mola_set_target_build_options TARGETNAME)
   if((NOT MSVC) AND ((NOT CMAKE_CROSSCOMPILING) AND (NOT CMAKE_BUILD_TYPE STREQUAL "Debug")))
     add_compile_options(${TARGETNAME} PRIVATE -O3 -mtune=native)
   endif()
+
 endfunction()
 
+# -----------------------------------------------------------------------------
+# mola_define_library(target)
+#
 # Define a consistent install behavior for cmake-based library project:
+# -----------------------------------------------------------------------------
 function(mola_define_library TARGETNAME)
   # Public hdrs interface:
   target_include_directories(${TARGETNAME} PUBLIC
@@ -53,6 +104,24 @@ function(mola_define_library TARGETNAME)
       $<INSTALL_INTERFACE:include>
       PRIVATE src
     )
+
+  # Dynamic libraries output options:
+  # -----------------------------------
+  set_target_properties(${TARGETNAME} PROPERTIES
+    OUTPUT_NAME "${TARGETNAME}${MOLA_DLL_VERSION_POSTFIX}"
+    COMPILE_PDB_NAME "${TARGETNAME}${MOLA_DLL_VERSION_POSTFIX}"
+    COMPILE_PDB_NAME_DEBUG "${TARGETNAME}${MOLA_DLL_VERSION_POSTFIX}${CMAKE_DEBUG_POSTFIX}"
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/"
+    LIBRARY_OUTPUT_DIRECTORY  "${CMAKE_BINARY_DIR}/lib/"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
+    VERSION "${MOLA_VERSION_NUMBER_MAJOR}.${MOLA_VERSION_NUMBER_MINOR}.${MOLA_VERSION_NUMBER_PATCH}"
+    SOVERSION ${MOLA_VERSION_NUMBER_MAJOR}.${MOLA_VERSION_NUMBER_MINOR}
+    )
+
+  # Project "folder":
+  # -------------------
+  set_target_properties(${TARGETNAME} PROPERTIES FOLDER "modules")
+
 
   # Install lib:
   install(TARGETS ${TARGETNAME} EXPORT ${TARGETNAME}Config
@@ -64,5 +133,5 @@ function(mola_define_library TARGETNAME)
   install(DIRECTORY include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
   # Install cmake config module
-  install(EXPORT ${TARGETNAME}Config DESTINATION share/${TARGETNAME}/cmake)  
+  install(EXPORT ${TARGETNAME}Config DESTINATION share/${TARGETNAME}/cmake)
 endfunction()
