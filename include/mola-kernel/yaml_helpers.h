e7bab6769673a49dadb40aca73c2d354477f910a
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-01-04 12:45:59 +0100

    more yaml helpers

--- include/mola-kernel/yaml_helpers.h
+++ include/mola-kernel/yaml_helpers.h
@@ -23,4 +23,27 @@ std::string parseEnvVars(const std::string& text);
 #define ENSURE_YAML_ENTRY_EXISTS(_c, _name) \
     ASSERTMSG_(_c[_name], "Missing YAML required entry: `" _name "`")
 
+/** Loads (optional) variable named "_varname" from the YAML config named `cfg`
+ * into the variable `_param_str._varname` */
+#define YAML_LOAD_OPT(_param_str, _varname, _type) \
+    _param_str._varname = cfg[#_varname].as<_type>(_param_str._varname)
+
+/** Loads (required) variable named "_varname" from the YAML config named `cfg`
+ * into the variable `_param_str._varname` */
+#define YAML_LOAD_REQ(_param_str, _varname, _type) \
+    ENSURE_YAML_ENTRY_EXISTS(cfg, #_varname);      \
+    YAML_LOAD_OPT(_param_str, _varname, _type)
+
+/** like YAML_LOAD_OPT, values in the YAML file in "degrees" stored in rads */
+#define YAML_LOAD_OPT_DEG(_param_str, _varname, _type)        \
+    _param_str._varname = mrpt::RAD2DEG(_param_str._varname); \
+    YAML_LOAD_OPT(_param_str, _varname, _type);               \
+    _param_str._varname = mrpt::DEG2RAD(_param_str._varname)
+
+/** like YAML_LOAD_REQ, values in the YAML file in "degrees" stored in rads */
+#define YAML_LOAD_REQ_DEG(_param_str, _varname, _type)        \
+    _param_str._varname = mrpt::RAD2DEG(_param_str._varname); \
+    YAML_LOAD_REQ(_param_str, _varname, _type);               \
+    _param_str._varname = mrpt::DEG2RAD(_param_str._varname)
+
 }  // namespace mola
