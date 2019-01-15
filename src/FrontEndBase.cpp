b88fbc17f191524c90e6d5e9f8b139befff025ad
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-01-15 12:46:33 +0100

    make raw_data_source optional

--- src/FrontEndBase.cpp
+++ src/FrontEndBase.cpp
@@ -31,24 +31,27 @@ void FrontEndBase::initialize_common(const std::string& cfg_block)
 
     MRPT_TODO("Enable multiple source modules, comma sep list, etc.");
     auto ds_source = cfg["raw_data_source"];
-    ASSERTMSG_(ds_source, "Missing parameter `raw_data_source`");
-    const auto src_name = ds_source.as<std::string>();
+    if (ds_source)
+    {
+        const auto src_name = ds_source.as<std::string>();
 
-    ASSERT_(this->nameServer_);
+        ASSERT_(this->nameServer_);
 
-    auto data_src = nameServer_(src_name);
-    if (!data_src)
-        THROW_EXCEPTION_FMT(
-            "Cannot find data source module named `%s`", src_name.c_str());
+        auto data_src = nameServer_(src_name);
+        if (!data_src)
+            THROW_EXCEPTION_FMT(
+                "Cannot find data source module named `%s`", src_name.c_str());
 
-    auto rdsb = std::dynamic_pointer_cast<RawDataSourceBase>(data_src);
-    if (!rdsb)
-        THROW_EXCEPTION_FMT(
-            "Could not cast data source module named `%s` to RawDataSourceBase",
-            src_name.c_str());
+        auto rdsb = std::dynamic_pointer_cast<RawDataSourceBase>(data_src);
+        if (!rdsb)
+            THROW_EXCEPTION_FMT(
+                "Could not cast data source module named `%s` to "
+                "RawDataSourceBase",
+                src_name.c_str());
 
-    // Subscribe:
-    rdsb->attachToDataConsumer(*this);
+        // Subscribe:
+        rdsb->attachToDataConsumer(*this);
+    }
 
     // Search for SLAM backend:
     auto fnd_bckends = ExecutableBase::findService<BackEndBase>();
