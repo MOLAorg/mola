bb0bb6982e558acab57a6263a3cb14754d95d936
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-08-31 10:01:48 +0200

    Update to mrpt 2.1 yaml

--- src/MolaViz.cpp
+++ src/MolaViz.cpp
@@ -18,7 +18,7 @@
 #include <mola-viz/MolaViz.h>
 #include <mrpt/core/initializer.h>
 #include <mrpt/opengl/CGridPlaneXY.h>
-#include <yaml-cpp/yaml.h>
+#include <mrpt/containers/yaml.h>
 
 using namespace mola;
 
@@ -56,7 +56,7 @@ void MolaViz::initialize(const std::string& cfg_block)
     MRPT_START
 
     // Load:
-    auto c   = YAML::Load(cfg_block);
+    auto c   = mrpt::containers::yaml::FromText(cfg_block);
     auto cfg = c["params"];
     MRPT_LOG_DEBUG_STREAM("Loading these params:\n" << cfg);
 
