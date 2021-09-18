94c739553f76407e3b3ab7adb0b1f42f54367cb4
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2021-09-18 12:45:04 +0200

    Initial commit

---
+++ src/Foo.cpp
@@ -0,0 +1,33 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2021 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   Foo.cpp
+ * @brief  XXX
+ * @author XXX
+ * @date   XXX
+ */
+
+/** \defgroup mola_foo_grp mola-foo
+ * Write me!
+ *
+ */
+
+#include <mola-foo/Foo.h>
+//#include <mrpt/containers/yaml.h>
+//#include <mrpt/core/initializer.h>
+
+using namespace mola;
+
+// Uncomment if you define RTTI/Serializable classes:
+/*
+// arguments: class_name, parent_class, class namespace
+IMPLEMENTS_MRPT_OBJECT(Foo, RawDataSourceBase, mola)
+
+MRPT_INITIALIZER(do_register_Foo)
+{
+    MOLA_REGISTER_MODULE(Foo);
+}
+*/
