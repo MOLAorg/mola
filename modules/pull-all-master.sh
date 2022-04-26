6ae47fd22ac463f7d03e9c4d01c6c1881e7f27f0
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2022-04-26 13:07:38 +0200

    Sync all

--- modules/pull-all-master.sh
+++ modules/pull-all-master.sh
@@ -4,9 +4,5 @@ MODS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $MODS_DIR
 
-# Ensure we are not in a detached commit (default after submodules clone):
-./git-all checkout master
-
-# Pull:
-./git-all pull
+./git-checkout-all-default-branch.sh
 
