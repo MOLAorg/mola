76ff57499688da3ee9e20e2b36a745d318224df1
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-09-08 10:15:56 +0200

    Initial commit

---
+++ include/mola-foo/Foo.h
@@ -0,0 +1,36 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2021 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   Foo.h
+ * @brief  XXX
+ * @author XXX
+ * @date   XXX
+ */
+#pragma once
+
+//#include <mola-kernel/interfaces/RawDataSourceBase.h>
+
+namespace mola
+{
+/** Write me!
+ *
+ * \ingroup mola_foo_grp
+ */
+class Foo // : public RawDataSourceBase
+{
+    //DEFINE_MRPT_OBJECT(Foo, mola)
+
+   public:
+    Foo() = default;
+    //~Foo() override = default; 
+
+    // See docs in base class
+//    void initialize(const std::string& cfg_block) override;
+//    void spinOnce() override;
+
+};
+
+}  // namespace mola
