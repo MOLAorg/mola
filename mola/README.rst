f36692c8ea5e2e47d39f1f842a763507c6e281d7
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2024-02-23 09:44:36 +0100

    add metapackage mola, and update badges table

---
+++ mola/README.rst
@@ -0,0 +1,6 @@
+MRPT 
+--------------------
+
+ROS wrappers for Mobile Robot Programming Toolkit (MRPT) classes and apps. 
+Refer to http://wiki.ros.org/mrpt_navigation for further documentation.
+
---
+++ scripts/generate_statuses_table.py
@@ -0,0 +1,37 @@
+#!/bin/env python3
+
+import os
+
+
+def list_directories(path):
+    # List all directories under the given path
+    directories = [d for d in os.listdir(
+        path) if os.path.isdir(os.path.join(path, d))]
+    return directories
+
+
+def extract_plain_name(directory):
+    # Extract the plain name without the full path
+    return os.path.basename(directory)
+
+
+def process_directories(path, items_to_remove, pattern_text):
+    directories = list_directories(path)
+
+    sorted_directories = sorted(directories, key=extract_plain_name)
+
+    for plain_name in sorted_directories:
+
+        # Check if the plain name passes the filter
+        if plain_name not in items_to_remove:
+            # Print the pattern text with the name
+            output_text = pattern_text.format(name=plain_name)
+            print(output_text)
+
+
+path_to_search = '..'
+items_to_remove = ['scripts', 'docs',
+                   '.vscode', '.github', '.circleci', '.git']
+pattern_text = "| {name} | [![Build Status](https://build.ros2.org/job/Hbin_uJ64__{name}__ubuntu_jammy_amd64__binary/badge/icon)](https://build.ros2.org/job/Hbin_uJ64__{name}__ubuntu_jammy_amd64__binary/) | [![Build Status](https://build.ros2.org/job/Ibin_uJ64__{name}__ubuntu_jammy_amd64__binary/badge/icon)](https://build.ros2.org/job/Ibin_uJ64__{name}__ubuntu_jammy_amd64__binary/)| [![Build Status](https://build.ros2.org/job/Rbin_uJ64__{name}__ubuntu_jammy_amd64__binary/badge/icon)](https://build.ros2.org/job/Rbin_uJ64__{name}__ubuntu_jammy_amd64__binary/) |"
+
+process_directories(path_to_search, items_to_remove, pattern_text)
