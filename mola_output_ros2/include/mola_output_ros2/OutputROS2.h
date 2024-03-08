a6a03f792a3b43c14f6e6b5f9b4b48e6c147fc7c
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2024-03-08 23:50:58 +0100

    ROS 2 output module

---
+++ mola_output_ros2/include/mola_output_ros2/OutputROS2.h
@@ -0,0 +1,132 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   OutputROS2.h
+ * @brief  Bridge MOLA->ROS2
+ * @author Jose Luis Blanco Claraco
+ * @date   Mar 7, 2024
+ */
+#pragma once
+
+#include <mola_kernel/interfaces/ExecutableBase.h>
+#include <mola_kernel/interfaces/RawDataConsumer.h>
+#include <mola_kernel/interfaces/RawDataSourceBase.h>
+#include <mrpt/obs/CObservationImage.h>
+#include <mrpt/obs/CObservationPointCloud.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <optional>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+namespace mola
+{
+/** Bridge: MOLA ==> ROS2
+ *
+ * This class can be used to:
+ *  - Publish datasets from any of the MOLA dataset input modules to ROS 2.
+ *  - Expose the results of a MOLA SLAM/odometry system to the rest of a ROS 2
+ *    system.
+ *
+ * \ingroup mola_output_ros2_grp
+ */
+class OutputROS2 : public mola::ExecutableBase, public mola::RawDataConsumer
+{
+    DEFINE_MRPT_OBJECT(OutputROS2, mola)
+
+   public:
+    OutputROS2();
+    ~OutputROS2() override = default;
+
+    // See docs in base class
+    void initialize(const Yaml& cfg) override;
+    void spinOnce() override;
+
+    // RawDataConsumer implementation:
+    void onNewObservation(const CObservation::Ptr& o) override;
+
+   private:
+    std::thread rosNodeThread_;
+
+    struct Params
+    {
+        /// tf frame name with respect to sensor poses are measured:
+        std::string base_link_frame = "base_footprint";
+
+        /// tf frame name for odometry's frame of reference:
+        std::string reference_frame = "map";
+
+        bool publish_odometry_msgs_from_slam = true;
+
+        /// If true, the original dataset timestamps will be used to publish.
+        /// Otherwise, the wallclock time will be used.
+        bool publish_in_sim_time = false;
+
+        double period_check_new_mola_subs = 1.0;  // [s]
+
+        unsigned int publisher_history_len = 10;
+    };
+
+    Params params_;
+
+    // Yaml is NOT a reference on purpose.
+    void ros_node_thread_main(Yaml cfg);
+
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_bc_;
+
+    rclcpp::Clock::SharedPtr ros_clock_;
+    std::mutex               ros_clock_mtx_;
+
+    std::shared_ptr<rclcpp::Node> rosNode_;
+    std::mutex                    rosNodeMtx_;
+
+    /// Returns a copy of the shared_ptr to the ROS 2 node, or empty if not
+    /// initialized yet.
+    std::shared_ptr<rclcpp::Node> rosNode()
+    {
+        auto lck = mrpt::lockHelper(rosNodeMtx_);
+        return rosNode_;
+    }
+
+    /// Returns either the wallclock "now" (params_.use_sim_time = false)
+    /// or the equivalent of the passed argument in ROS 2 format otherwise.
+    rclcpp::Time myNow(const mrpt::Clock::time_point& observationStamp);
+
+    struct RosPubs
+    {
+        /// Publisher of "odom" topic
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odom;
+        /// "base_pose_ground_truth" topic
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_ground_truth;
+
+        /// Map <sensor_label> => publisher
+        std::map<std::string, rclcpp::PublisherBase::SharedPtr> pub_sensors;
+
+        // rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+        // pub_markers;
+    };
+
+    RosPubs    rosPubs_;
+    std::mutex rosPubsMtx_;
+
+    struct MolaSubs
+    {
+        std::set<mola::RawDataSourceBase::Ptr> dataSources;
+    };
+
+    MolaSubs   molaSubs_;
+    std::mutex molaSubsMtx_;
+
+    double lastTimeCheckMolaSubs_ = 0;
+    void   doLookForNewMolaSubs();
+
+    void internalOn(const mrpt::obs::CObservationImage& obs);
+    void internalOn(const mrpt::obs::CObservationPointCloud& obs);
+};
+
+}  // namespace mola
---
+++ mola_output_ros2/src/OutputROS2.cpp
@@ -0,0 +1,336 @@
+/* -------------------------------------------------------------------------
+ *   A Modular Optimization framework for Localization and mApping  (MOLA)
+ * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * See LICENSE for license information.
+ * ------------------------------------------------------------------------- */
+/**
+ * @file   OutputROS2.h
+ * @brief  Bridge MOLA->ROS2
+ * @author Jose Luis Blanco Claraco
+ * @date   Mar 7, 2024
+ */
+
+/** \defgroup mola_output_ros2_grp mola_output_ros2_grp.
+ * Bridge: MOLA ==> ROS2
+ *
+ */
+
+// MOLA interfaces:
+#include <mola_kernel/pretty_print_exception.h>
+
+// me:
+#include <mola_output_ros2/OutputROS2.h>
+
+// MOLA/MRPT:
+#include <mola_yaml/yaml_helpers.h>
+#include <mrpt/containers/yaml.h>
+#include <mrpt/core/initializer.h>
+#include <mrpt/maps/CPointsMapXYZI.h>
+#include <mrpt/maps/CPointsMapXYZIRT.h>
+#include <mrpt/maps/CSimplePointsMap.h>
+#include <mrpt/obs/CObservation2DRangeScan.h>
+#include <mrpt/obs/CObservationOdometry.h>
+#include <mrpt/obs/CObservationPointCloud.h>
+#include <mrpt/ros2bridge/image.h>
+#include <mrpt/ros2bridge/laser_scan.h>
+#include <mrpt/ros2bridge/point_cloud2.h>
+#include <mrpt/ros2bridge/pose.h>
+#include <mrpt/ros2bridge/time.h>
+#include <mrpt/system/filesystem.h>
+
+// ROS 2:
+#include <rclcpp/node.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+using namespace mola;
+
+// arguments: class_name, parent_class, class namespace
+IMPLEMENTS_MRPT_OBJECT(OutputROS2, mola::ExecutableBase, mola)
+
+MRPT_INITIALIZER(do_register_OutputROS2) { MOLA_REGISTER_MODULE(OutputROS2); }
+
+OutputROS2::OutputROS2()
+{
+    mrpt::system::COutputLogger::setLoggerName("OutputROS2");
+}
+
+// The ROS node starts with MOLA::initialize() and ends with its dtor
+void OutputROS2::ros_node_thread_main([[maybe_unused]] Yaml cfg)
+{
+    using std::placeholders::_1;
+    using namespace std::string_literals;
+
+    const char* NODE_NAME = "mola_output_ros2";
+
+    try
+    {
+        const int         argc    = 1;
+        char const* const argv[2] = {NODE_NAME, nullptr};
+
+        // Initialize ROS:
+        rclcpp::init(argc, argv);
+
+        auto lckNode = mrpt::lockHelper(rosNodeMtx_);
+
+        rosNode_ = std::make_shared<rclcpp::Node>(NODE_NAME);
+
+        lckNode.unlock();
+
+        {
+            auto lck   = mrpt::lockHelper(ros_clock_mtx_);
+            ros_clock_ = rosNode_->get_clock();
+        }
+
+        tf_bc_ = std::make_shared<tf2_ros::TransformBroadcaster>(rosNode_);
+
+        // Spin:
+        rclcpp::spin(rosNode_);
+        rclcpp::shutdown();
+    }
+    catch (const std::exception& e)
+    {
+        mola::pretty_print_exception(e, "OutputROS2::ros_node_thread_main");
+    }
+}
+
+void OutputROS2::initialize(const Yaml& c)
+{
+    using namespace std::string_literals;
+
+    MRPT_START
+    ProfilerEntry tle(profiler_, "initialize");
+
+    // Mandatory parameters:
+    ENSURE_YAML_ENTRY_EXISTS(c, "params");
+    auto cfg = c["params"];
+
+    std::stringstream ss;
+    cfg.printAsYAML(ss);
+    const Yaml cfgCopy = Yaml::FromStream(ss);
+
+    MRPT_LOG_DEBUG_STREAM("Initializing with these params:\n" << cfgCopy);
+
+    // General params:
+    YAML_LOAD_OPT(params_, base_link_frame, std::string);
+    YAML_LOAD_OPT(params_, reference_frame, std::string);
+    YAML_LOAD_OPT(params_, publish_odometry_msgs_from_slam, bool);
+    YAML_LOAD_OPT(params_, publish_in_sim_time, bool);
+
+    // Launch ROS node:
+    rosNodeThread_ =
+        std::thread(&OutputROS2::ros_node_thread_main, this, cfgCopy);
+
+    MRPT_END
+}  // end initialize()
+
+void OutputROS2::spinOnce()
+{
+    using mrpt::system::timeDifference;
+
+    MRPT_START
+    ProfilerEntry tleg(profiler_, "spinOnce");
+
+    // Check for new mola data sources?
+    if (mrpt::Clock::nowDouble() - lastTimeCheckMolaSubs_ >
+        params_.period_check_new_mola_subs)
+    {
+        lastTimeCheckMolaSubs_ = mrpt::Clock::nowDouble();
+        doLookForNewMolaSubs();
+    }
+
+    MRPT_END
+}
+
+void OutputROS2::onNewObservation(const CObservation::Ptr& o)
+{
+    using namespace mrpt::obs;
+
+    ASSERT_(o);
+
+    // TODO(jlbc): Add some filter not to publish everything to ROS?
+
+    if (auto oImg = std::dynamic_pointer_cast<CObservationImage>(o); oImg)
+    {
+        return internalOn(*oImg);
+    }
+    else if (auto oPc = std::dynamic_pointer_cast<CObservationPointCloud>(o);
+             oPc)
+    {
+        return internalOn(*oPc);
+    }
+    else
+    {
+        MRPT_LOG_THROTTLE_WARN_FMT(
+            5.0,
+            "Do not know how to publish to ROS an observation of type '%s' "
+            "with sensorLabel='%s'",
+            o->GetRuntimeClass()->className, o->sensorLabel.c_str());
+    }
+}
+
+void OutputROS2::internalOn(const mrpt::obs::CObservationImage& obs)
+{
+    auto lck = mrpt::lockHelper(rosPubsMtx_);
+
+    // Create the publisher the first time an observation arrives:
+    const bool is_1st_pub = rosPubs_.pub_sensors.find(obs.sensorLabel) ==
+                            rosPubs_.pub_sensors.end();
+    auto& pub = rosPubs_.pub_sensors[obs.sensorLabel];
+
+    if (is_1st_pub)
+    {
+        pub = rosNode()->create_publisher<sensor_msgs::msg::Image>(
+            obs.sensorLabel, params_.publisher_history_len);
+    }
+    lck.unlock();
+
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pubImg =
+        std::dynamic_pointer_cast<rclcpp::Publisher<sensor_msgs::msg::Image>>(
+            pub);
+    ASSERT_(pubImg);
+
+    const std::string sSensorFrameId = obs.sensorLabel;
+
+    // Send TF:
+    mrpt::poses::CPose3D sensorPose;
+    obs.getSensorPose(sensorPose);
+
+    tf2::Transform transform = mrpt::ros2bridge::toROS_tfTransform(sensorPose);
+
+    geometry_msgs::msg::TransformStamped tfStmp;
+    tfStmp.transform       = tf2::toMsg(transform);
+    tfStmp.child_frame_id  = sSensorFrameId;
+    tfStmp.header.frame_id = params_.base_link_frame;
+    tfStmp.header.stamp    = myNow(obs.timestamp);
+    tf_bc_->sendTransform(tfStmp);
+
+    // Send observation:
+    {
+        obs.load();
+
+        // Convert observation MRPT -> ROS
+        sensor_msgs::msg::Image msg_img;
+        std_msgs::msg::Header   msg_header;
+        msg_header.stamp    = myNow(obs.timestamp);
+        msg_header.frame_id = sSensorFrameId;
+
+        msg_img = mrpt::ros2bridge::toROS(obs.image, msg_header);
+
+        pubImg->publish(msg_img);
+    }
+}
+
+void OutputROS2::internalOn(const mrpt::obs::CObservationPointCloud& obs)
+{
+    using namespace std::string_literals;
+
+    auto lck = mrpt::lockHelper(rosPubsMtx_);
+
+    const auto lbPoints = obs.sensorLabel + "_points"s;
+
+    // Create the publisher the first time an observation arrives:
+    const bool is_1st_pub =
+        rosPubs_.pub_sensors.find(lbPoints) == rosPubs_.pub_sensors.end();
+
+    auto& pubPts = rosPubs_.pub_sensors[lbPoints];
+
+    if (is_1st_pub)
+    {
+        pubPts = rosNode()->create_publisher<sensor_msgs::msg::PointCloud2>(
+            lbPoints, params_.publisher_history_len);
+    }
+    lck.unlock();
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pubPoints =
+        std::dynamic_pointer_cast<
+            rclcpp::Publisher<sensor_msgs::msg::PointCloud2>>(pubPts);
+    ASSERT_(pubPoints);
+
+    const std::string sSensorFrameId_points = lbPoints;
+
+    // POINTS
+    // --------
+
+    const std::string sSensorFrameId = obs.sensorLabel;
+
+    // Send TF:
+    mrpt::poses::CPose3D sensorPose = obs.sensorPose;
+
+    tf2::Transform transform = mrpt::ros2bridge::toROS_tfTransform(sensorPose);
+
+    geometry_msgs::msg::TransformStamped tfStmp;
+    tfStmp.transform       = tf2::toMsg(transform);
+    tfStmp.child_frame_id  = sSensorFrameId;
+    tfStmp.header.frame_id = params_.base_link_frame;
+    tfStmp.header.stamp    = myNow(obs.timestamp);
+    tf_bc_->sendTransform(tfStmp);
+
+    // Send observation:
+    {
+        // Convert observation MRPT -> ROS
+        sensor_msgs::msg::PointCloud2 msg_pts;
+        std_msgs::msg::Header         msg_header;
+        msg_header.stamp    = myNow(obs.timestamp);
+        msg_header.frame_id = sSensorFrameId;
+
+        obs.load();
+
+        if (auto* xyzirt = dynamic_cast<const mrpt::maps::CPointsMapXYZIRT*>(
+                obs.pointcloud.get());
+            xyzirt)
+        {
+            mrpt::ros2bridge::toROS(*xyzirt, msg_header, msg_pts);
+        }
+        else if (auto* xyzi = dynamic_cast<const mrpt::maps::CPointsMapXYZI*>(
+                     obs.pointcloud.get());
+                 xyzi)
+        {
+            mrpt::ros2bridge::toROS(*xyzi, msg_header, msg_pts);
+        }
+        else if (auto* sPts = dynamic_cast<const mrpt::maps::CSimplePointsMap*>(
+                     obs.pointcloud.get());
+                 sPts)
+        {
+            mrpt::ros2bridge::toROS(*sPts, msg_header, msg_pts);
+        }
+        else
+        {
+            THROW_EXCEPTION(
+                "Do not know how to handle this variant of CPointsMap");
+        }
+
+        pubPoints->publish(msg_pts);
+    }
+}
+
+void OutputROS2::doLookForNewMolaSubs()
+{
+    auto lck = mrpt::lockHelper(molaSubsMtx_);
+
+    auto listRDS = this->findService<mola::RawDataSourceBase>();
+    for (auto& module : listRDS)
+    {
+        auto rds = std::dynamic_pointer_cast<mola::RawDataSourceBase>(module);
+        ASSERT_(rds);
+
+        if (molaSubs_.dataSources.count(rds) == 0)
+        {
+            MRPT_LOG_INFO_STREAM(
+                "Subscribing to MOLA data source module '"
+                << rds->getModuleInstanceName() << "'");
+
+            // a new one:
+            molaSubs_.dataSources.insert(rds);
+            rds->attachToDataConsumer(*this);
+        }
+    }
+}
+
+rclcpp::Time OutputROS2::myNow(const mrpt::Clock::time_point& observationStamp)
+{
+    if (params_.publish_in_sim_time)
+        return mrpt::ros2bridge::toROS(observationStamp);
+    else
+        return mrpt::ros2bridge::toROS(mrpt::Clock::now());
+}
