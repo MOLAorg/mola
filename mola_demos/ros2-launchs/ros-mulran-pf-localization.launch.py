47266e2d1553d706fdc2cdc0e328663f0c213401
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-03-25 20:17:25 +0100

    reorganized ros2 files

--- mola_demos/ros2-launchs/ros-mulran-pf-localization.launch.py
+++ mola_demos/ros2-launchs/ros-mulran-pf-localization.launch.py
@@ -14,10 +14,23 @@ import os
 def generate_launch_description():
     myDir = get_package_share_directory("mola_demos")
 
-    pf_params_file = os.path.join(myDir, 'ros2-launchs', 'mrpt-pf.yaml')
+    pf_params_file = os.path.join(myDir, 'ros2-params', 'mrpt-pf.yaml')
 
     pointcloud_pipeline_file = os.path.join(
-        myDir, 'ros2-launchs', 'point-cloud-pipeline.yaml')
+        myDir, 'ros2-params', 'point-cloud-pipeline.yaml')
+
+    # TODO(jlbc): Automatic download from the web for the demo:
+    reference_map_file = './map_KAIST01_georef.mm'
+
+    # Launch map server:
+    mrpt_map_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mrpt_map_server'), 'launch',
+            'mrpt_map_server.launch.py')]),
+        launch_arguments={
+            'mm_file': reference_map_file,
+        }.items()
+    )
 
     # Launch for pf_localization:
     mrpt_pf_localization_launch = IncludeLaunchDescription(
@@ -32,7 +45,7 @@ def generate_launch_description():
         }.items()
     )
 
-    # Launch for MULRAN dataset:
+    # Launch for MULRAN dataset playback MOLA->ROS:
     mulran_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             myDir, 'ros2-launchs', 'ros-mulran-play.launch.py')]),
@@ -52,7 +65,6 @@ def generate_launch_description():
             'points_topic_name': '/lidar_points',
             'filter_output_layer_name': 'output_for_pf',
             'filter_output_topic_name': '/pf_input_points',
-            'time_window': '0.20',
             'show_gui': 'False',
             'one_observation_per_topic': 'True',
             'frameid_robot': 'base_link',
@@ -61,7 +73,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        mrpt_pf_localization_launch,
         mulran_launch,
+        mrpt_map_launch,
         pointcloud_pipeline_launch,
+        mrpt_pf_localization_launch,
     ])
