7e7f1ec4a9fe31a8e763b93774d548b8c0909210
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-02-11 12:12:33 +0100

    Add utility RegexCache

---
+++ mola_kernel/include/mola_kernel/utils/RegexCache.h
@@ -0,0 +1,51 @@
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ Copyright (C) 2018-2026 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: GPL-3.0
+ See LICENSE for full license information.
+ Closed-source licenses available upon request, for this odometry package
+ alone or in combination with the complete SLAM system.
+*/
+
+/**
+ * @file   RegexCache.h
+ * @brief  Cached regular expression
+ * @author Jose Luis Blanco Claraco
+ * @date   Jan 29, 2026
+ */
+#pragma once
+
+#include <optional>
+#include <regex>
+#include <string>
+
+namespace mola
+{
+
+/** Holds a reference to a cached regex. Recompiles only if the expression changed.
+ */
+class RegexCache
+{
+ public:
+  // Returns a reference to a cached regex. Recompiles only if the expression changed.
+  const std::regex& get_regex(const std::string& regExpression)
+  {
+    if (!cachedRegex_ || regExpression != cachedExpression_)
+    {
+      cachedExpression_ = regExpression;
+      cachedRegex_.emplace(cachedExpression_, std::regex::ECMAScript);
+    }
+    return *cachedRegex_;
+  }
+
+ private:
+  std::string               cachedExpression_;
+  std::optional<std::regex> cachedRegex_;
+};
+
+}  // namespace mola
